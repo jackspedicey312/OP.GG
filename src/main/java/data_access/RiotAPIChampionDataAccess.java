45196e88b4bdd761c0d2e8e778b2201b8975debb
pwatana <patricia.watanabe@mail.utoronto.ca>
2024-11-29 18:08:58 -0500

    Champion data access modified, files deleted

--- src/main/java/data_access/RiotAPIChampionDataAccess.java
+++ src/main/java/data_access/RiotAPIChampionDataAccess.java
@@ -13,21 +13,31 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
+/***
+ * Fetches champion data and calculates mastery points from the Riot API.
+ */
 public class RiotAPIChampionDataAccess {
+
     private static final String API_KEY = "RGAPI-f4800267-6eb1-45a5-89d8-b130ffff4f87";
     private String summonerID;
     private String region;
 
+    /**
+     * To create a new data access object to fetch champion data.
+     *
+     * @param summonerID The unique identified of the summoner.
+     * @param region The region fo the summoner ("NA", "EU", "ASIA").
+     */
     public RiotAPIChampionDataAccess(String summonerID, String region) {
         this.summonerID = summonerID;
         this.region = region;
     }
 
-    public void setSummonerIDAndRegion(String summonerID, String region) {
-        this.summonerID = summonerID;
-        this.region = region;
-    }
-
+    /**
+     * Fetch champion data for all champions for the summoner.
+     * @return A list of ChampionOutputData objects containing champion data.
+     * @throws IOException If no champion found in data.
+     */
     public List<ChampionOutputData> fetchAllChampions() throws IOException {
         List<ChampionOutputData> championDataList = new ArrayList<>();
 
@@ -36,31 +46,41 @@ public class RiotAPIChampionDataAccess {
 
         if (responseCode == HttpURLConnection.HTTP_OK) {
             try (BufferedReader in = new BufferedReader(new InputStreamReader(request.getInputStream()))) {
-                final JSONArray championInfoArray = new JSONArray(new JSONTokener(in));
+                final JSONObject matchDetails = new JSONObject(new JSONTokener(in));
+                final JSONObject info = matchDetails.getJSONObject("info");
+                final JSONArray participants = info.getJSONArray("participants");
 
-                for (int i = 0; i < championInfoArray.length(); i++) {
-                    JSONObject championData = championInfoArray.getJSONObject(i);
+                for (int i = 0; i < participants.length(); i++) {
+                    JSONObject participant = participants.getJSONObject(i);
 
-                    String championName = championData.getString("championName");
-                    int championId = championData.getInt("championId");
-                    int magicDamage = championData.getInt("magicDamageDealt");
-                    int physicalDamage = championData.getInt("physicalDamageDealt");
-                    int totalDamage = championData.getInt("totalDamageDealt");
-                    int trueDamage = championData.getInt("trueDamageDealt");
-                    int kills = championData.getInt("kills");
+                    String championName = participant.getString("championName");
+                    int championId = participant.getInt("championId");
+                    int magicDamage = participant.getInt("magicDamageDealt");
+                    int physicalDamage = participant.getInt("physicalDamageDealt");
+                    int totalDamage = participant.getInt("totalDamageDealt");
+                    int trueDamage = participant.getInt("trueDamageDealt");
+                    int kills = participant.getInt("kills");
 
-                    int masteryPoints = calculateMasteryPoints(
+                    int masteryPoints = CalculateMastery.calculateMasteryPoints(
                             totalDamage, magicDamage, physicalDamage, trueDamage, kills
                     );
 
                     ChampionOutputData championOutput = new ChampionOutputData(
-                            championName, championId, magicDamage, physicalDamage, totalDamage, trueDamage, kills, masteryPoints
+                            championName,
+                            championId,
+                            magicDamage,
+                            physicalDamage,
+                            totalDamage,
+                            trueDamage,
+                            kills,
+                            masteryPoints
                     );
 
                     championDataList.add(championOutput);
                 }
             }
-        } else {
+        }
+        else {
             throw new IOException("HTTP error code: " + responseCode);
         }
 
@@ -71,11 +91,14 @@ public class RiotAPIChampionDataAccess {
         final String baseURL;
         if (region.equalsIgnoreCase("NA")) {
             baseURL = "https://na1.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/";
-        } else if (region.equalsIgnoreCase("EU")) {
+        }
+        else if (region.equalsIgnoreCase("EU")) {
             baseURL = "https://euw1.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/";
-        } else if (region.equalsIgnoreCase("ASIA")) {
+        }
+        else if (region.equalsIgnoreCase("ASIA")) {
             baseURL = "https://kr.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/";
-        } else {
+        }
+        else {
             throw new IllegalArgumentException("Unsupported region: " + region);
         }
 
@@ -85,26 +108,6 @@ public class RiotAPIChampionDataAccess {
         request.setRequestMethod("GET");
         request.setRequestProperty("X-Riot-Token", API_KEY);
         return request;
-    }
-
-    private int calculateMasteryPoints(int totalDamage, int magicDamage, int physicalDamage, int trueDamage, int kills) {
-        final int totalDamageMax = 100000;
-        final int magicDamageMax = 50000;
-        final int physicalDamageMax = 90000;
-        final int trueDamageMax = 2000;
-        final int killsMax = 30;
-
-        int normTotalDamage = normalize(totalDamage, 0, totalDamageMax);
-        int normMagicDamage = normalize(magicDamage, 0, magicDamageMax);
-        int normPhysicalDamage = normalize(physicalDamage, 0, physicalDamageMax);
-        int normTrueDamage = normalize(trueDamage, 0, trueDamageMax);
-        int normKills = normalize(kills, 0, killsMax);
-
-        return normTotalDamage + normMagicDamage + normPhysicalDamage + normTrueDamage + normKills;
-    }
-
-    private int normalize(int value, int min, int max) {
-        return (int) (((double) (value - min) / (max - min)) * 1000);
     }
 }
 
