682ca50624f32b24cdaac657047afa33ab9e0848
lejas <kha.le@mail.utoronto.ca>
2024-11-30 01:17:47 -0500

    Logout

---
+++ src/main/java/use_case/logout/LogoutInteractor.java
@@ -0,0 +1,21 @@
+package use_case.logout;
+
+/**
+ * The Logout Interactor.
+ */
+public class LogoutInteractor implements LogoutInputBoundary {
+    private UserDataAccessInterface userDataAccessObject;
+    private LogoutOutputBoundary logoutPresenter;
+
+    public LogoutInteractor(UserDataAccessInterface userDataAccessInterface,
+                            LogoutOutputBoundary logoutOutputBoundary) {
+        this.userDataAccessObject = userDataAccessInterface;
+        this.logoutPresenter = logoutOutputBoundary;
+    }
+
+    @Override
+    public void execute(LogoutInputData logoutInputData) {
+        userDataAccessObject.setCurrentUsername(null);
+        logoutPresenter.prepareSuccessView(new LogoutOutputData(logoutInputData.getUsername(), false));
+    }
+}
