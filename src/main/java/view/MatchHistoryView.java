79965598be92275dd9381cfef5c6cea6d23dd439
Bryan <bryand0617@gmail.com>
2024-11-30 21:00:13 -0500

    Finish the MatchHistoryView

--- src/main/java/view/MatchHistoryView.java
+++ src/main/java/view/MatchHistoryView.java
@@ -2,10 +2,17 @@ package view;
 
 import javax.swing.*;
 import java.awt.*;
+
+import entity.match.Match;
+import entity.matchHistory.MatchHistory;
 import interface_adapter.back.BackController;
+import interface_adapter.login.LoginState;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
+import java.util.List;
 
 public class MatchHistoryView extends JPanel implements ActionListener, PropertyChangeListener {
     private final String viewName = "MatchHistory";
@@ -25,11 +32,84 @@ public class MatchHistoryView extends JPanel implements ActionListener, Property
 
         this.mainPanel = new JPanel(new BorderLayout());
         this.listPanel = new JPanel(new BorderLayout());
+        listPanel.setBorder(BorderFactory.createTitledBorder("Match History"));
+        listPanel.setLayout(new BoxLayout(listPanel, BoxLayout.Y_AXIS));
         this.buttonPanel = new JPanel(new BorderLayout());
         backbutton.addActionListener(this);
         mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
         this.mainPanel.add(listPanel);
         this.mainPanel.add(buttonPanel);
+    }
+
+    public void propertyChange(PropertyChangeEvent evt) {
+        final MatchHistory matches = matchHistoryViewModel.getstate().getMatchHistory();
+        final int length = matches.getLength();
+        listPanel.removeAll();
+        for (int i = 0; i < length; i++) {
+            final Match match = matches.getMatch(i);
+            final JPanel eachmatch = createMatchDetails(match);
+            listPanel.add(eachmatch);
+        }
+    }
+
+    @SuppressWarnings({"checkstyle:MethodName", "checkstyle:SuppressWarnings"})
+    private JPanel createMatchDetails(Match match) {
+        JPanel backPanel = new JPanel();
+        backPanel.setLayout(new BoxLayout(backPanel, BoxLayout.X_AXIS));
+        JPanel leftPanel = new JPanel();
+
+        leftPanel.setLayout(new BoxLayout(leftPanel, BoxLayout.Y_AXIS));
+        JPanel leftupPanel = new JPanel();
+        leftupPanel.setLayout(new BoxLayout(leftupPanel, BoxLayout.Y_AXIS));
+        JLabel aram = new JLabel(match.getGameMode());
+        JLabel date = new JLabel(match.getDate());
+        leftPanel.add(aram);
+        leftPanel.add(date);
+
+        JPanel leftmiddlePanel = new JPanel();
+
+        JPanel leftdownPanel = new JPanel();
+        leftdownPanel.setLayout(new BoxLayout(leftdownPanel, BoxLayout.Y_AXIS));
+        JLabel result = new JLabel(match.getResult());
+        JLabel duration = new JLabel(match.getDuration());
+        leftdownPanel.add(result);
+        leftdownPanel.add(duration);
+
+        leftPanel.add(leftupPanel);
+        leftPanel.add(leftmiddlePanel);
+        leftPanel.add(leftdownPanel);
+
+        JPanel middlePanel = new JPanel();
+        middlePanel.setLayout(new BoxLayout(middlePanel, BoxLayout.Y_AXIS));
+        JLabel icon = new JLabel(match.getChampionIcon());
+        middlePanel.add(icon);
+
+        JPanel rightPanel = new JPanel();
+        rightPanel.setLayout(new BoxLayout(rightPanel, BoxLayout.Y_AXIS));
+        JPanel rightupPanel = new JPanel();
+        JPanel rightmiddlePanel = new JPanel();
+        JLabel kda = new JLabel(getKDA(match));
+        rightmiddlePanel.add(kda);
+        JPanel rightdownPanel = new JPanel();
+
+        rightPanel.add(rightupPanel);
+        rightPanel.add(rightmiddlePanel);
+        rightPanel.add(rightdownPanel);
+
+        backPanel.add(leftPanel);
+        backPanel.add(middlePanel);
+        backPanel.add(rightPanel);
+        return backPanel;
+    }
+
+    private String getKDA(Match match) {
+        int k = match.getKills();
+        int d = match.getDeaths();
+        int a = match.getAssissts();
+        String strk = String.valueOf(k);
+        String strd = String.valueOf(d);
+        String stra = String.valueOf(a);
+        return strk + "/" + strd + "/" + stra;
     }
 
     public void actionPerformed(ActionEvent e) {
@@ -38,6 +118,10 @@ public class MatchHistoryView extends JPanel implements ActionListener, Property
 
     public String getViewName() {
         return viewName;
+    }
+
+    public void setBackController(BackController backController) {
+        this.backController = backController;
     }
 
 }
