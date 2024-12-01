59b69585d2fb54033b47d8335e8f8c90e6c9c269
lejas <kha.le@mail.utoronto.ca>
2024-12-01 05:00:04 -0500

    Champion Finished

---
+++ src/main/java/interface_adapter/champion/ChampionPresenter.java
@@ -0,0 +1,20 @@
+package interface_adapter.champion;
+
+import interface_adapter.ViewManagerModel;
+import use_case.champion.ChampionOutputBoundary;
+
+public class ChampionPresenter implements ChampionOutputBoundary {
+    private final ChampionViewModel championViewModel;
+    private final ViewManagerModel viewManagerModel;
+
+    public ChampionPresenter(ChampionViewModel championViewModel,
+                                         ViewManagerModel viewmanagerModel) {
+        this.championViewModel = championViewModel;
+        this.viewManagerModel = viewmanagerModel;
+    }
+
+    public void prepareView() {
+        viewManagerModel.setState(championViewModel.getViewName());
+        viewManagerModel.firePropertyChanged();
+    }
+}
