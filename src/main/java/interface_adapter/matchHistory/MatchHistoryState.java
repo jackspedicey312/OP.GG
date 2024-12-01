cfeb20f34fec22a4f55e4763458d8701d9046254
jackspedicey <jack9426li@hotmail.com>
2024-11-30 22:35:41 -0500

    Changed Match History state to get info from entity as requested by the view

--- src/main/java/interface_adapter/matchHistory/MatchHistoryState.java
+++ src/main/java/interface_adapter/matchHistory/MatchHistoryState.java
@@ -2,11 +2,41 @@ package interface_adapter.matchHistory;
 
 import entity.matchHistory.MatchHistory;
 
+import javax.swing.ImageIcon;
+
 public class MatchHistoryState {
     private MatchHistory matchHistory;
 
     public MatchHistory getMatchHistory() {
         return matchHistory;
+    }
+
+    public ImageIcon getChampionIcon(int index) {
+        return matchHistory.getMatch(index).getChampionIcon();
+    }
+
+    public int getKills(int index) {
+        return matchHistory.getMatch(index).getKills();
+    }
+
+    public int getDeaths(int index) {
+        return matchHistory.getMatch(index).getDeaths();
+    }
+
+    public int getAssissts(int index) {
+        return matchHistory.getMatch(index).getAssissts();
+    }
+
+    public String getDuration(int index) {
+        return matchHistory.getMatch(index).getDuration();
+    }
+
+    public String getGameMode(int index) {
+        return matchHistory.getMatch(index).getGameMode();
+    }
+
+    public String getDate(int index) {
+        return matchHistory.getMatch(index).getDate();
     }
 
     public void setMatchHistory(MatchHistory matchHistory) {
