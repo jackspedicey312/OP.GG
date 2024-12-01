69d479d32daf63c46ef7fbaba6c561f758db1eac
jhao5 <j.hao@mail.utoronto.ca>
2024-11-30 23:22:51 -0500

    clean arch for FunFact complete use case not complete

--- src/main/java/interface_adapter/funfacts/FunFactPresenter.java
+++ src/main/java/interface_adapter/funfacts/FunFactPresenter.java
@@ -1,113 +1,22 @@
 package interface_adapter.funfacts;
 
+import interface_adapter.ViewManagerModel;
+import interface_adapter.matchHistory.MatchHistoryViewModel;
 import use_case.funfacts.FunFactsOutputBoundary;
 
 public class FunFactPresenter implements FunFactsOutputBoundary {
-    private int totalPlaytime;
-    private int totalWins;
-    private int totalLosses;
-    private int totalKills;
-    private int totalDeaths;
-    private String oldestGamePlayed;
-    private int longestGamePlayed;
-    private String longestGamePlayedDate;
-    private int totalSurrenders;
-    private int totalPentakills;
-    private int totalSurvivedSingleDigitHp;
-    private int totalSnowballsHit;
-    private int totalSavedAllies;
 
-    public void presentTotalPlaytime(int Playtime) {
-        this.totalPlaytime = Playtime;
+    private final FunFactViewModel funFactViewModel;
+    private final ViewManagerModel viewManagerModel;
+
+    public FunFactPresenter(FunFactViewModel funFactViewModel, ViewManagerModel viewManagerModel) {
+        this.funFactViewModel = funFactViewModel;
+        this.viewManagerModel = viewManagerModel;
     }
 
-    public void presentTotalWins(int wins) {
-        this.totalWins = wins;
-    }
-
-    public void presentTotalLosses(int loss) {
-        this.totalLosses = loss;
-    }
-
-    public void presentTotalKills(int kills) {
-        this.totalKills = kills;
-    }
-
-    public void presentTotalDeaths(int deaths) {
-        this.totalDeaths = deaths;
-    }
-
-    public void presentLongestGamePlayed(int longestGame) {
-        this.longestGamePlayed = longestGame;
-    }
-
-    public void presentLongestGamePlayedDate(String longestGameDate) {
-        this.longestGamePlayedDate = longestGameDate;
-    }
-
-    public void presentOldestGamePlayed(String oldestGame) {
-        this.oldestGamePlayed = oldestGame;
-    }
-
-    public void presentTotalSurvivedSingleDigitHp(int survivedSingleDigitHp) {
-        this.totalSurvivedSingleDigitHp = survivedSingleDigitHp;
-    }
-
-    public void presentTotalSurrenders(int Surrenders) {
-        this.totalSurrenders = Surrenders;
-    }
-
-    public void presentTotalPentakills(int Pentakills) {
-        this.totalPentakills = Pentakills;
-    }
-
-    public void presentTotalSnowballs(int totalSnowballs) {
-        this.totalSnowballsHit = totalSnowballs;
-    }
-
-    public void presentTotalSavedAllies(int savedAllies) {
-        this.totalSavedAllies = savedAllies;
-    }
-
-    public int getTotalPlaytime() {
-        return totalPlaytime;
-    }
-
-    public int getTotalWins() {
-        return totalWins;
-    }
-    public int getTotalLosses() {
-        return totalLosses;
-    }
-    public int getTotalKills() {
-        return totalKills;
-    }
-    public int getTotalDeaths() {
-        return totalDeaths;
-    }
-
-    public String getOldestGamePlayed() {
-        return oldestGamePlayed;
-    }
-    public int getLongestGamePlayed() {
-        return longestGamePlayed;
-    }
-    public String getLongestGamePlayedDate() {
-        return longestGamePlayedDate;
-    }
-    public int getTotalSurrenders() {
-        return totalSurrenders;
-    }
-    public int getTotalPentakills() {
-        return totalPentakills;
-    }
-    public int getTotalSurvivedSingleDigitHp() {
-        return totalSurvivedSingleDigitHp;
-    }
-    public int getTotalSnowballsHit() {
-        return totalSnowballsHit;
-    }
-    public int getTotalSavedAllies() {
-        return totalSavedAllies;
+    @Override
+    public void prepareView() {
+        viewManagerModel.setState(funFactViewModel.getViewName());
+        viewManagerModel.firePropertyChanged();
     }
 }
--- src/main/java/interface_adapter/funfacts/FunFactViewModel.java
+++ src/main/java/interface_adapter/funfacts/FunFactViewModel.java
@@ -1,4 +1,11 @@
 package interface_adapter.funfacts;
 
-public class FunFactViewModel {
+import interface_adapter.ViewModel;
+
+public class FunFactViewModel extends ViewModel<FunFactState> {
+
+    public FunFactViewModel() {
+        super("Fun Facts");
+        setState(new FunFactState());
+    }
 }
