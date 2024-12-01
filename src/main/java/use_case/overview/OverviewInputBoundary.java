841428d8e48dea841291c1155e98b17bc096349e
jhao5 <j.hao@mail.utoronto.ca>
2024-12-01 00:25:04 -0500

    overview integrated into riotapp

--- src/main/java/use_case/overview/OverviewInputBoundary.java
+++ src/main/java/use_case/overview/OverviewInputBoundary.java
@@ -2,8 +2,6 @@ package use_case.overview;
 
 public interface OverviewInputBoundary {
 
-    void fetchOverview(String puuid, String region);
-
     void execute();
 
 }
--- src/main/java/use_case/overview/OverviewUseCase.java
+++ src/main/java/use_case/overview/OverviewUseCase.java
@@ -1,53 +1,15 @@
 package use_case.overview;
 
-import data_access.RiotAPIProfileDataAccess;
-import data_access.RiotAPIRankDataAccess;
-import entity.OverviewProfile.ProfileOverview;
-import entity.OverviewProfile.Rank;
-
-import java.io.IOException;
-
 public class OverviewUseCase implements OverviewInputBoundary {
 
-    private final RiotAPIProfileDataAccess profiledataAccess;
-    private final OverviewOutputBoundary presenter;
-    private final RiotAPIRankDataAccess rankDataAccess;
+    private final OverviewOutputBoundary overviewPresenter;
 
-    // NEEDS DATA ACCESS AND PRESENTER TO INITIALIZE NO MATTER WHAT.
-    public OverviewUseCase(RiotAPIProfileDataAccess profileDataAccess, RiotAPIRankDataAccess rankDataAccess,
-                           OverviewOutputBoundary presenter) {
-        this.profiledataAccess = profileDataAccess;
-        this.rankDataAccess = rankDataAccess;
-        this.presenter = presenter;
-    }
-
-    public void fetchOverview(String puuid, String region) {
-        try {
-
-            ProfileOverview profileOverview = profiledataAccess.generateProfileData(puuid, region);
-            presenter.presentProfileIcon(profileOverview.getSummonerImage());
-            presenter.presentProfileLevel(profileOverview.getSummonerLevel());
-
-            final String summonerId = profileOverview.getSummonerId();
-            Rank rank = rankDataAccess.generateRank(summonerId, region);
-
-            presenter.presentGamemode(rank.getGameMode());
-            presenter.presentRank(rank.getRank());
-            presenter.presentDivision(rank.getDivision());
-            presenter.presentWins(rank.getWins());
-            presenter.presentLosses(rank.getLosses());
-            presenter.presentLeaguePoints(rank.getLeaguePoints());
-            presenter.presentWinRate(rank.getWinRate());
-            presenter.presentRankIcon(rank.getRankImage());
-
-        }
-        catch (IOException e) {
-            presenter.presentError("Failed to fetch profile" + e.getMessage());
-        }
+    public OverviewUseCase(OverviewOutputBoundary overviewPresenter) {
+        this.overviewPresenter = overviewPresenter;
     }
 
     @Override
     public void execute() {
-
+        overviewPresenter.prepareView();
     }
 }
